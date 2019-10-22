4ca4f212bcbbf721af88127ad020e4bef37819b9
Hector De Diego <h.dediego@distillery.com>
2019-10-22 15:42:25 -0500

    Initial Commit

---
+++ contactFixTests/contactFixTests.swift
@@ -0,0 +1,34 @@
+//
+//  contactFixTests.swift
+//  contactFixTests
+//
+//  Created by Hector De Diego on 22/10/19.
+//  Copyright © 2019 dediego. All rights reserved.
+//
+
+import XCTest
+@testable import contactFix
+
+class contactFixTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
